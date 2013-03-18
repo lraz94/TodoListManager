fcbbc19252ece62e3089d9bf210d2f850f495113
lraz94 <lior.raz@mail.huji.ac.il>
2013-03-18 05:26:28 +0200

    ex3

---
+++ TodoListManager/src/il/ac/huji/todolist/AddNewTodoItemActivity.java
@@ -0,0 +1,49 @@
+package il.ac.huji.todolist;
+
+import java.util.Calendar;
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+public class AddNewTodoItemActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_add_new_todo_item);
+		final EditText editTextNewItem = (EditText) findViewById(R.id.edtNewItem);
+		final DatePicker datePicker = (DatePicker) findViewById(R.id.datePicker);
+		Button buttonOk = (Button) findViewById(R.id.btnOK);
+		buttonOk.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				Intent result = new Intent();
+				Calendar cal = Calendar.getInstance();
+				int year = datePicker.getYear();
+				int month = datePicker.getMonth();
+				int day = datePicker.getDayOfMonth();
+				cal.clear();
+				cal.set(Calendar.YEAR,year);
+				cal.set(Calendar.MONTH, month);
+				cal.set(Calendar.DAY_OF_MONTH,day);
+				result.putExtra("dueDate",cal.getTime());
+				result.putExtra("title",editTextNewItem.getText().toString());
+				setResult(RESULT_OK,result);
+				finish();
+			}
+		});
+		Button buttonCancel = (Button) findViewById(R.id.btnCancel);
+		buttonCancel.setOnClickListener(new OnClickListener() {
+			public void onClick(View arg0) {
+				setResult(RESULT_CANCELED);
+				finish();
+			}
+		});
+	}
+
+
+}
--- TodoListManager/src/il/ac/huji/todolist/TodoListManagerActivity.java
+++ TodoListManager/src/il/ac/huji/todolist/TodoListManagerActivity.java
@@ -1,35 +1,46 @@
 package il.ac.huji.todolist;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.graphics.Color;
+import android.view.ContextMenu;
+import android.view.ContextMenu.ContextMenuInfo;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView.AdapterContextMenuInfo;
 import android.widget.ArrayAdapter;
-import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 
 public class TodoListManagerActivity extends Activity {
 
+	public static final int CODE_FOR_ADD_NEW = 100;
 	private CustomListAdapter _adapter;
-	private EditText _editNewItem;
 	private ListView _list;
+	private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("dd/MM/yyyy",Locale.US);
+	private static final String STRING_FOR_NO_DATE = "No due date";
+	private static final String CALL = "Call: ";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_todo_list_manager);
-		_editNewItem = (EditText) findViewById(R.id.edtNewItem);
 		_list = (ListView)findViewById(R.id.lstTodoItems);
-		_adapter = new CustomListAdapter(this,new ArrayList<String>());
+		_adapter = new CustomListAdapter(this,new ArrayList<TaskDatePair>());
 		_list.setAdapter(_adapter);
+		registerForContextMenu(_list);
 	}
 
 	@Override
@@ -44,14 +55,64 @@ public class TodoListManagerActivity extends Activity {
 		super.onOptionsItemSelected(item);   
 		switch (item.getItemId()){
 		case R.id.menuItemAdd : {
-			String added = _editNewItem.getText().toString();
-			_adapter.add(added);
+			Intent intent = new Intent(this, AddNewTodoItemActivity.class);
+			startActivityForResult(intent, CODE_FOR_ADD_NEW);
 			break;
 		}
-		case R.id.menuItemDelete :{
-			int positionDeleted = _list.getSelectedItemPosition();
-			if(positionDeleted!=-1){
-				_adapter.remove(_adapter.getItem(positionDeleted));
+		}
+		return true;
+	}
+
+
+	protected void onActivityResult(int reqCode, int resCode, Intent data) {
+		if (reqCode==CODE_FOR_ADD_NEW && resCode == RESULT_OK){
+			if(data!=null){
+				String title = data.getStringExtra("title");
+				Date date = null;
+				Object dateObj = data.getSerializableExtra("dueDate");
+				if(dateObj!=null){
+					date = (Date) dateObj;
+				}
+				_adapter.add(new TaskDatePair(title,date));
+				// TODO delete syso
+				System.out.println("added new to adapter");
+			}
+		}
+	}
+
+
+	public void onCreateContextMenu(ContextMenu menu, View v, ContextMenuInfo info) {
+		getMenuInflater().inflate(R.menu.ctxmenuextended, menu);
+		int pos = ((AdapterContextMenuInfo) info).position;
+		TaskDatePair taskPair = _adapter.getItem(pos);
+		String taskStr = taskPair.get_task();
+		menu.setHeaderTitle(taskStr);
+		MenuItem callItem = menu.findItem(R.id.menuItemCall);
+		if(!taskStr.contains(CALL)){
+			callItem.setVisible(false);
+		}
+		else{
+			callItem.setTitle(taskStr);
+		}
+		
+		super.onCreateContextMenu(menu,v,info);
+	}
+
+	public boolean onContextItemSelected(MenuItem item) {
+		AdapterContextMenuInfo info = (AdapterContextMenuInfo) item.getMenuInfo();
+		int pos = info.position;
+		TaskDatePair taskPair = _adapter.getItem(pos);
+		switch (item.getItemId()){
+		case R.id.menuItemDelete:{
+			_adapter.remove(taskPair);
+			break;
+		}
+		case R.id.menuItemCall:{
+			String gotFromTask = taskPair.get_task();
+			if(gotFromTask.startsWith(CALL) && gotFromTask.length() > CALL.length()){
+				gotFromTask = gotFromTask.substring(CALL.length());
+				Intent dial = new Intent(Intent.ACTION_DIAL,Uri.parse("tel:"+gotFromTask));
+				startActivity(dial);
 			}
 			break;
 		}
@@ -60,29 +121,66 @@ public class TodoListManagerActivity extends Activity {
 	}
 
 
-	private class CustomListAdapter extends ArrayAdapter<String> {
+	private class CustomListAdapter extends ArrayAdapter<TaskDatePair> {
+		public CustomListAdapter(TodoListManagerActivity activity, List<TaskDatePair> tasks) {	
+			super(activity, android.R.layout.simple_list_item_1, tasks);
 
-
-		public CustomListAdapter(TodoListManagerActivity activity, List<String> courses) {	
-			super(activity, android.R.layout.simple_list_item_1, courses);
-			
 		}
 
 		@Override
 		public View getView(int position, View v, ViewGroup parent)    {
-			String strItem = getItem(position);
+			TaskDatePair item = getItem(position);
 			LayoutInflater inflater = LayoutInflater.from(getContext());
 			View view = inflater.inflate(R.layout.row, null);
-			TextView task = (TextView) view.findViewById(R.id.textTask);
-			task.setText(strItem);
-			if(position%2==0){
-				task.setTextColor(Color.RED);
-			}	
+			TextView title = (TextView) view.findViewById(R.id.txtTodoTitle);
+			title.setText(item.get_task());
+			TextView dateTxt = (TextView) view.findViewById(R.id.txtTodoDueDate);
+			Date dateGot = item.get_date();
+			if(dateGot!=null){
+				dateTxt.setText(DATE_FORMAT.format(dateGot));
+				// this date
+				Calendar now = Calendar.getInstance();
+				Calendar toCompare = Calendar.getInstance();
+				toCompare.clear();
+				toCompare.set(Calendar.YEAR,now.get(Calendar.YEAR));
+				toCompare.set(Calendar.MONTH,now.get(Calendar.MONTH));
+				toCompare.set(Calendar.DAY_OF_MONTH,now.get(Calendar.DAY_OF_MONTH));
+				Calendar other = Calendar.getInstance();
+				other.setTime(dateGot);
+				Calendar gotDateToComapre = Calendar.getInstance();
+				gotDateToComapre.clear();
+				gotDateToComapre.set(Calendar.YEAR,other.get(Calendar.YEAR));
+				gotDateToComapre.set(Calendar.MONTH,other.get(Calendar.MONTH));
+				gotDateToComapre.set(Calendar.DAY_OF_MONTH,other.get(Calendar.DAY_OF_MONTH));
+				if(gotDateToComapre.before(toCompare)){
+					title.setTextColor(Color.RED);
+					dateTxt.setTextColor(Color.RED);
+				}
+			}
 			else{
-				task.setTextColor(Color.BLUE);
+				dateTxt.setText(STRING_FOR_NO_DATE);
 			}
 			return view;
 		}
 
+	}
+
+	private static class TaskDatePair{
+
+		private Date _date;
+		private String _task;
+
+		public TaskDatePair(String task,Date date){
+			_task = task;
+			_date = date;
+		}
+
+		public Date get_date() {
+			return _date;
+		}
+
+		public String get_task() {
+			return _task;
+		}
 	}
 }
